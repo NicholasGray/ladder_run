50a1509a76a1ee81e97e153f40f7acbc575fc4a9
Nicholas Gray <nicholas_gray@live.com>
2025-06-04 16:18:31 -0400

    Update Play scene with ladder drawing and climb tweens

--- public/game.js
+++ public/game.js
@@ -40,8 +40,43 @@ class Play extends Phaser.Scene {
     super('Play');
   }
 
+  preload() {
+    this.load.image('ladder', 'ladder.png');
+    this.load.image('brick-bg', 'brick-bg.png');
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    for (const c of colors) {
+      this.load.image(`avatar-${c}`, `avatar-${c}.png`);
+    }
+  }
+
   create() {
     this.add.text(100, 100, 'Waiting for game...', { fontSize: '32px', fill: '#fff' });
+    socket.on('snapshot', (state) => this.drawState(state));
+    socket.on('climb', ({ teamId, rung }) => {
+      const sprite = this.avatarSprites && this.avatarSprites[teamId];
+      if (sprite) {
+        this.tweens.add({ targets: sprite, y: this.yForRung(rung), duration: 300 });
+      }
+    });
+  }
+
+  yForRung(r) {
+    return 530 - r * 40;
+  }
+
+  drawState(snapshot) {
+    this.add.image(400, 300, 'brick-bg');
+    this.avatarSprites = [];
+    for (let i = 0; i < ladderX.length; i++) {
+      this.add.image(ladderX[i], 300, 'ladder');
+    }
+
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    for (let i = 0; i < snapshot.teams.length; i++) {
+      const team = snapshot.teams[i];
+      const sprite = this.add.sprite(ladderX[i], this.yForRung(team.rung || 0), `avatar-${colors[i]}`);
+      this.avatarSprites[i] = sprite;
+    }
   }
 }
 
@@ -52,5 +87,6 @@ class End extends Phaser.Scene {
 }
 
 const socket = io();
+const ladderX = [100, 260, 420, 580, 740];
 const config = { type: Phaser.AUTO, width: 800, height: 600, scene: [Lobby, Play, End] };
 new Phaser.Game(config);
