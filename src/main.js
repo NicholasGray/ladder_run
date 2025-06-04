702b730bddb53900d2fb28e743e743a197e1bec2
Nicholas Gray <nicholas_gray@live.com>
2025-06-04 14:52:08 -0400

    Initial Phaser game setup

---
+++ src/main.js
@@ -0,0 +1,37 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.setBaseURL('https://labs.phaser.io');
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+}
+
+function create () {
+    this.add.image(400, 300, 'sky');
+    var logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+}
+
+function update () {
+}
